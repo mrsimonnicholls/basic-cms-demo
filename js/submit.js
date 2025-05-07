3fee600c4c76f55122c7464a670c9384e52ee1d1
mrsimonnicholls <simon.nicholls@mediadesignschool.com>
2025-05-07 17:46:52 +1200

    update

--- js/submit.js
+++ js/submit.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://vdtufuffqepkagsnltev.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkdHVmdWZmcWVwa2Fnc25sdGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1NzI1ODYsImV4cCI6MjA2MjE0ODU4Nn0.fFoB5letPd02tRGT35Cw-LB-8puiZbOihsXahUwcbsU';
+const SUPABASE_KEY = 'your-anon-key';
 
 document.getElementById('postForm').addEventListener('submit', async (e) => {
   e.preventDefault();
